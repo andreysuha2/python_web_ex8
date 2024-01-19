c0cc3e5b55d6953e605c4908d508f63ba1dea124
Andrii Sukhenko <andreysuha2@gmail.com>
2024-01-19 20:44:18 +0200

    Add redis cache

--- ex_8/search.py
+++ ex_8/search.py
@@ -1,6 +1,10 @@
 from ex_8.connect import connect
 from ex_8.models import Quote, Author
+import json
+import os
+import redis
 
+r = redis.Redis(host=os.getenv("REDIS_HOST"), port=int(os.getenv("REDIS_PORT")), password=os.getenv("REDIS_PASSWORD"))
 
 def search_by_name(name: str) -> list[Quote]:
     authors = Author.objects(fullname__istartswith=name).all()
@@ -10,7 +14,7 @@ def search_by_tag(tag: str) -> list[Quote]:
     return Quote.objects(tags__icontains=tag, tags__istartswith=tag).all()
 
 def search_by_tags(tags_str: str) -> list[Quote]:
-    tags = [ tag.strip() for tag in tags_str.split(',') ]
+    tags = tags_str.split(",")
     return Quote.objects(tags__in=tags).all()
 
 queries = {
@@ -21,10 +25,18 @@ queries = {
 
 def pars_search_str(string: str) -> tuple[str, str]:
     key, value = string.split(":")
-    return key.strip(), value.strip()
+    return key.strip(), ",".join([ item.strip() for item in value.split(",") ])
 
 def pars_quote(quote: Quote) -> str:
     return f"{quote.quote}; autor: {quote.author.fullname}; tags: {', '.join(quote.tags)}"
+
+def load_quotes(query, key, value):
+    redis_key = f"{key}:{value}"
+    if not r.exists(redis_key):
+        quotes = query(value)
+        r.set(redis_key, json.dumps([ pars_quote(quote) for quote in quotes ]), ex=86400)
+    return json.loads(r.get(redis_key))
+    
 
 def main() -> None:
     try:
@@ -37,10 +49,10 @@ def main() -> None:
             if not query:
                 print(f"You cant find somesing by key: {key}. Please use name, tag or tags as kay")
             else:
-                quotes = query(value)
+                quotes = load_quotes(query, key, value)
                 if len(quotes):
                     for quote in quotes:
-                        print(pars_quote(quote))
+                        print(quote)
                         print("")
                 else:
                     print(f"For params '{search}' not found results!")
